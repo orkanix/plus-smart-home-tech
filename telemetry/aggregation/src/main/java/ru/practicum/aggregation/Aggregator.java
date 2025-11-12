830f2d764d2aa7be2a4711b41e829aaa6a3af96e
orkanix <sockolov.44itya@yandex.ru>
2025-11-12 13:07:11 +0300

    Реализован сервис Aggregator.

---
+++ telemetry/aggregation/src/main/java/ru/practicum/aggregation/Aggregator.java
@@ -0,0 +1,24 @@
+package ru.practicum.aggregation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.ConfigurableApplicationContext;
+import ru.practicum.aggregation.consumer.AggregationStarter;
+
+@SpringBootApplication
+@ConfigurationPropertiesScan
+
+public class Aggregator {
+
+    public static void main(String[] args) {
+        // Запуск Spring Boot приложения при помощи вспомогательного класса SpringApplication
+        // метод run возвращает назад настроенный контекст, который мы можем использовать для
+        // получения настроенных бинов
+        ConfigurableApplicationContext context = SpringApplication.run(Aggregator.class, args);
+
+        // Получаем бин AggregationStarter из контекста и запускаем основную логику сервиса
+        AggregationStarter aggregator = context.getBean(AggregationStarter.class);
+        aggregator.start();
+    }
+}
--- telemetry/serialization/avro-schemas/src/main/java/ru/practicum/avro/deserializer/SensorEventDeserializer.java
+++ telemetry/serialization/avro-schemas/src/main/java/ru/practicum/avro/deserializer/SensorEventDeserializer.java
@@ -1,10 +1,16 @@
 package ru.practicum.avro.deserializer;
 
 import org.apache.avro.Schema;
+import org.springframework.stereotype.Component;
 import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
 
+@Component
 public class SensorEventDeserializer extends BaseAvroDeserializer<SensorEventAvro> {
     public SensorEventDeserializer(Schema schema) {
         super(schema);
+    }
+
+    public SensorEventDeserializer() {
+        super(SensorEventAvro.getClassSchema()); // используем статический метод Avro
     }
 }
